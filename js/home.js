03e6881af2eed5c313221ec1237b0cb5ba2af713
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-09-01 12:53:24 +0530

    shoeCart

---
+++ js/home.js
@@ -0,0 +1,6 @@
+const nav_part3 = document.querySelector(".nav-part3")
+const menu = document.querySelector(".menu")
+
+menu.addEventListener("click",()=>{
+    nav_part3.classList.toggle('displayNavItems')
+})
